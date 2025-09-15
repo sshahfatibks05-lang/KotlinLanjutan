a8a5d82d21be1bb83f8be83725987150309b3a2b
sshahfatibks05-lang <sshahfatibks05@gmail.com>
2025-09-15 23:06:51 +0700

    Ini adalah Mutable List Of

---
+++ src/09MutableListOf.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val numbers = mutableListOf(1, 2, 3, 4)
+    numbers.add(5)
+    numbers.removeAt(1)
+    numbers[0] = 0
+    numbers.shuffle()
+    println(numbers)
+
+    val numbersStr = mutableListOf("one", "two", "three", "four")
+    numbersStr.add("five")
+    println(numbersStr)
+}
