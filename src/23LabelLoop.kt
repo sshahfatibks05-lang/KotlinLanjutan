8e0d2e68b0ee32e765323fcfe1d0e513200bbb9f
sshahfatibks05-lang <sshahfatibks05@gmail.com>
2025-09-17 09:43:53 +0700

    Ini adalah Label pada For-Loop

---
+++ src/23LabelLoop.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    println("Example of Break and Continue Label")
+    myLabel@ for(x in 1..10) {
+        if(x == 5) {
+            println("I am inside if block with value"+x+"\n-- hence it will close the operation")
+            break@myLabel //specifing the label
+        } else {
+            println("I am inside else block with value"+x)
+            continue@myLabel
+        }
+        println("Not Print")
+    }
+}
